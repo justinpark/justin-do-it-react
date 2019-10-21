23964a42538e8bdefdedc1eebf96d7434b260475
Justin Park <justin.park@airbnb.com>
2019-10-20 20:18:18 -0700

    App 참조 파일

---
+++ src/03/DefaultPropsExample.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import DefaultPropsComponent from './03/DefaultPropsComponent';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <DefaultPropsComponent />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/03/StateExampleApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import StateExample from './03/StateExample’;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <StateExample />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
