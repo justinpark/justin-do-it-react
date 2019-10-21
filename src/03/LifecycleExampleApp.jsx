5fe9f0fb5811a5334aebc938b3f624c842727cc3
Justin Park <justin.park@airbnb.com>
2019-10-20 22:29:26 -0700

    LifecycleExample App예제

---
+++ src/03/LifecycleExampleApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import LifecycleExample from './03/LifecycleExample';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <LifecycleExample />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
