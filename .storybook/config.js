31ea3c8be32ee43c3983cba29cea69538ff90cd8
Justin Park <justin.park@airbnb.com>
2019-10-26 15:37:23 -0700

    스토리북 scss경로 수정

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
 import JSXAddon from 'storybook-addon-jsx';
 
 // 04-2 스타일 적용할 때 사용된 파일
-// import '../src/materialize.scss';
+// import '../src/sass/materialize.scss';
 import '../src/doit-ui/app.css';
 
 function loadStories() {
