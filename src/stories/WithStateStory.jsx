7c4ce87ed8125547c90845cfb2c43757457a8627
Justin Park <justin.park@airbnb.com>
2019-11-08 10:54:54 -0800

    하이어오더 컴포넌트없는 카운터 예제 추가

--- src/stories/WithStateStory.jsx
+++ src/stories/WithStateStory.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { CounterWithCountState, CounterWithCountHandler } from '../05/withState';
+import Counter from '../03/Counter';
 
 storiesOf('WithState', module)
   .addWithJSX('CounterWithCountState', () => <CounterWithCountState />)
-  .addWithJSX('CounterWithCountHandler', () => <CounterWithCountHandler />);
+  .addWithJSX('CounterWithCountHandler', () => <CounterWithCountHandler />)
+  .addWithJSX('CounterWithoutHoC', () => <Counter />);
