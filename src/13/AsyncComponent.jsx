3beddef8848a65fb671baf1d187819aa775ff215
Justin Park <justin.park@airbnb.com>
2019-12-20 08:18:26 -0800

    13장 코드 변경

--- src/13/AsyncComponent.jsx
+++ src/13/AsyncComponent.jsx
@@ -14,12 +14,13 @@ export default class AsyncComponent extends PureComponent {
   render() {
     const { loader, ...otherProps } = this.props;
     const Component = this.Component;
-    if (Component) {
-      return <Component {...otherProps} />;
-    }
 
-    return (
-      <Text secondary large>로딩중...</Text>
+    return Component ? (
+      <Component {...otherProps} />
+    ) : (
+      <Text secondary large>
+        로딩중...
+      </Text>
     );
   }
 }
