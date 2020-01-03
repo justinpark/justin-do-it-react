63c85dacbbe24935abbfb2f48fec79d3317c5f9b
Justin Park <justin.park@airbnb.com>
2020-01-03 13:41:36 -0800

    13장 수정

--- src/08/Api.js
+++ src/08/Api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
+const isDev = process.env.NODE_ENV === 'development';
 const Api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development'
-    ? 'http://localhost:4000/'
-    : '/api/',
+  baseURL: isDev ? 'http://localhost:4000/' : '/api/',
 });
 
 export default Api;
