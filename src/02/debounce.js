ec261f10c43f3372c3e201343ae2fcc9e1d21159
Just[in]Do it! <justin.park@airbnb.com>
2020-07-05 19:41:28 -0700

    Update debounce.js

--- src/02/debounce.js
+++ src/02/debounce.js
@@ -1,12 +1,11 @@
 export function debounce(func, delay) {
   let inDebounce;
   return function(...args) {
-    const context = this;
     if (inDebounce) {
       clearTimeout(inDebounce);
     }
     inDebounce = setTimeout(
-      () => func.call(context, ...args),
+      () => func(...args),
       delay);
   }
 }
